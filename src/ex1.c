9d3e39ada6a5039c905a475e5ebac5b839d1a396
ArjoniDiego <diegoarjoni@gmail.com>
2022-02-07 14:16:26 -0300

    Primeiro Rep certo

---
+++ src/ex1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/* Enunciado:
+ *
+ * Elabore um programa que exiba "Ola, mundo!" na tela e pula uma linha
+ *
+ */
+
+int main (int argc, char *argv[])
+{
+  printf("Olá, mundo!\n");
+  
+  return 0;
+}
